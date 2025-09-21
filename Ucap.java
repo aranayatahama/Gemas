2ab038b56adf1dab7970157c0a8f972184a19e86
aranayatahama <aryatamaarya217@gmail.com>
2025-09-21 20:08:20 +0800

    Add files via upload

---
+++ Ucap.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package salam;
+
+/**
+ *
+ * @author Arya
+ */
+public class Ucap {
+
+    public static void main(String[] args) {
+        System.out.println("Halo ULM");
+        System.out.println("ULM Berdaya Saing dan Unggul di Bidang Lahan Basah");
+        System.out.println("Prodi Ilkom ULM Unggul dengan Teknologi Kekinian");
+    }
+}
